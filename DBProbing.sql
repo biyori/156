65373a98ebce85df1e9663462818150e946d8d76
Kyle Andrews <kyleandrews@mail.fresnostate.edu>
2021-12-01 18:14:06 -0800

    Formatted probes

--- DBProbing.sql
+++ DBProbing.sql
@@ -2,10 +2,18 @@
 -- .mode column
 
 -- View the auction winners (highest bidders) for each item
-select id, item_id, price, client_uuid from auction_winners where price = (select max(price) from auction_winners i where i.item_id = auction_winners.item_id);
+select id, item_id, price, client_uuid from auction_winners where price = (select max(price) from auction_winners i
+where i.item_id = auction_winners.item_id);
 
 -- View the total spend for bidder
-select sum(price), client_uuid from (select id, item_id, price, client_uuid from auction_winners where price = (select max(price) from auction_winners i where i.item_id = auction_winners.item_id)) group by client_uuid;
+select sum(price), client_uuid from (select id, item_id, price, client_uuid from auction_winners
+where price = (select max(price) from auction_winners i where i.item_id = auction_winners.item_id))
+group by client_uuid;
 
 -- View the total items each bidder purchased
-select count(item_id), client_uuid from (select id, item_id, price, client_uuid from auction_winners where price = (select max(price) from auction_winners i where i.item_id = auction_winners.item_id)) group by client_uuid;
+select count(item_id), client_uuid from (select id, item_id, price, client_uuid from auction_winners
+where price = (select max(price) from auction_winners i where i.item_id = auction_winners.item_id))
+group by client_uuid;
+
+
+
